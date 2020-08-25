33cbd3e8ceaa87f7e26e4e4fcfdf3e0850f904db
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2020-08-25 15:21:55 +0500

    Add raw-loader for shaders

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -484,6 +484,10 @@ module.exports = function(webpackEnv) {
                 },
                 'sass-loader'
               ),
+            },// Require .glsl as raw text.
+            {
+              test: /\.glsl$/i,
+              use: 'raw-loader',
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
